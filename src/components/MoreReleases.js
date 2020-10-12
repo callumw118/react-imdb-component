3d0e3dd4eefd37855b2de48ab580ab7613070143
Callum Wolfe <wolfe.callum@gmail.com>
2020-10-12 19:24:19 +0100

    remove typo

--- src/components/MoreReleases.js
+++ src/components/MoreReleases.js
@@ -2,6 +2,6 @@ import React from 'react';
 
 export default function MoreReleases(){
     return (
-        <button><a href='https://www.imdb.com/calendar/?region=gb'>View more upcoming releases >></a></button>
+        <button><a href='https://www.imdb.com/calendar/?region=gb'>View more upcoming releases</a></button>
     )
 }
